d9e09c098deb237dcc0008fa8c1884665b2194de
Alexandre Castello <aaca91@gmail.com>
2020-06-09 17:21:04 -0300

    test

--- app/policies/pet_policy.rb
+++ app/policies/pet_policy.rb
@@ -1,11 +1,11 @@
-class PetPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
+# class PetPolicy < ApplicationPolicy
+#   class Scope < Scope
+#     def resolve
+#       scope.all
+#     end
 
-    def update?
-      record.user == user
-    end
-  end
-end
+#     def update?
+#       record.user == user
+#     end
+#   end
+# end

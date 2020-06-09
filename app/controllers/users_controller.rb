1af7b49556e8dd69032c4dd04835b4c6895ff078
Edgar Pellizzon <edgpelizzon@gmail.com>
2020-06-09 11:55:32 -0300

    routes to user pages and validate changes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
+  def show
+    @spotteds = Pet.spotted(:id).where(user: @user)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "User updated"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
+    params.require(:user).permit(%i[email name password first_name last_name photo])
+  end
+
+end

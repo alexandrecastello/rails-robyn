62415a8df2b56d2748aed156dd5bb60351cbd249
Alexandre Castello <aaca91@gmail.com>
2020-06-10 11:51:42 -0300

    spotted working. spotted policy need adjustments

--- app/controllers/spotteds_controller.rb
+++ app/controllers/spotteds_controller.rb
@@ -1,11 +1,20 @@
 class SpottedsController < ApplicationController
+  skip_after_action :verify_authorized, only: %i[new]
+
   def new
-    @spotted = Spotted.new    
+    @spotted = Spotted.new   
+    @pet = Pet.find(params[:pet_id]) 
   end
   
 
   def create
     @spotted = Spotted.new(spotted_params)
+    @spotted.pet = Pet.find(params[:pet_id])
+    @spotted.user = current_user
+    #IMPLEMENTAR API para pegar longitude e latitude
+    @spotted.save
+    redirect_to pet_path(@spotted.pet)
+    authorize @spotted
   end
 
   private

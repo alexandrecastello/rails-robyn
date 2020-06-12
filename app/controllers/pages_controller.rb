681e847d8cc0f5310bf4fab6de09337d184e8b6c
Alexandre Castello <aaca91@gmail.com>
2020-06-12 17:32:36 -0300

    fixed home exib

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @pets = Pet.all.size > 1 ? Pet.all[-1..Pet.all.size] : Pet.all
+    @pets = Pet.last(4)
   end
 
   def my_profile

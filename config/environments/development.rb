1fc32122eddcee1cec311278f66a66b9e98f2c18
Alexandre Castello <aaca91@gmail.com>
2020-06-17 17:56:13 -0300

    viewerJS implemented

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "8d6c9ac60dca.ngrok.io"
+  config.hosts << "313d65e50ddc.ngrok.io"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.

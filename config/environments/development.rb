2ea175370daeca569fc8b143445f688ab6bd374e
Edgar Pellizzon <edgpelizzon@gmail.com>
2020-06-18 15:45:04 -0300

    google mailer implement - w/o google app password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://robyn.com.br/" }
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "6a31f935577e.ngrok.io"
   # In the development environment your application's code is reloaded on
